0fd1b169392ee1d6951acf724eabf449842e6766
sarthak kumar <sarthakkumar10151@gmail.com>
2025-05-19 02:51:21 +0530

    add nav and footer to color palette

--- src/ui/Colors/ColorPalette.tsx
+++ src/ui/Colors/ColorPalette.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import { notFound } from "next/navigation";
 import { ColorBox } from "../Palette/ColorBox";
 import { useAppContextActions } from "../context/useContext";
+import { Footer, Navbar } from "../components";
 
 type Props = {
 	paletteId: string;
@@ -16,6 +17,10 @@ export const ColorPalette = (props: Props) => {
 	const palette = React.useMemo(() => actions.getPalette(paletteId), [paletteId, actions]);
 	const color = React.useMemo(() => actions.getColor(paletteId, colorId), [paletteId, colorId, actions]);
 
+	const handleFormatChange = React.useCallback((format: string) => {
+		console.log({ format });
+	}, []);
+
 	if (!palette || !color) {
 		notFound();
 	}
@@ -26,14 +31,9 @@ export const ColorPalette = (props: Props) => {
 
 	return (
 		<main className="h-full w-full flex flex-col m-0 p-0">
-			{/* header */}
-			<header className="w-full h-[40px] flex justify-center items-center ">
-				<h1 className="font-bold text-2xl text-cyan-700">
-					{palette.paletteName} - {color.name}
-				</h1>
-			</header>
-			{/* color boxes */}
+			<Navbar onColorFromatChange={handleFormatChange} />
 			<div className="w-full flex-1 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 auto-rows-fr">{colors}</div>
+			<Footer displayName={`${palette.paletteName} ${palette.emoji}`} />
 		</main>
 	);
 };
--- src/ui/components/Navbar.tsx
+++ src/ui/components/Navbar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import React from "react";
 
 type Props = {
-	showShadeControls: boolean;
+	showShadeControls?: boolean;
 	onColorLevelChange?: (level: number) => void;
 	colorLevel?: number;
 	colorFormat?: string;
