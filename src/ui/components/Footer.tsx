4dfe4ee113e03dd63eb9aceaca6dd34624cd7210
sarthak kumar <sarthakkumar10151@gmail.com>
2025-05-20 03:03:56 +0530

    changed size for footer

--- src/ui/components/Footer.tsx
+++ src/ui/components/Footer.tsx
@@ -6,7 +6,7 @@ type Props = {
 export const Footer = (props: Props) => {
 	
 			return <footer className="h-[50px] px-[20px] flex justify-between items-center">
-				<h1 className="font-bold text-2xl text-cyan-700">
+				<h1 className="font-bold text-xl text-cyan-700">
 					{props.displayName}
 				</h1>
 			</footer>
