c82a205554d45d1a1eefb32fad2c0f028040ab48
sarthak kumar <sarthakkumar10151@gmail.com>
2025-05-28 21:55:41 +0530

    change footer

--- src/ui/components/Footer.tsx
+++ src/ui/components/Footer.tsx
@@ -4,10 +4,9 @@ type Props = {
 	displayName: string;
 }
 export const Footer = (props: Props) => {
-	
-			return <footer className="h-[50px] px-[20px] flex justify-between items-center">
-				<h1 className="font-bold text-xl text-cyan-700">
-					{props.displayName}
-				</h1>
-			</footer>
+	return (
+		<footer className="h-[50px] px-[20px] flex justify-between items-center bg-teal-100 text-white">
+			<h1 className="font-bold text-xl text-cyan-900">{props.displayName}</h1>
+		</footer>
+	);
 };
